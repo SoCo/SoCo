c09ad03aed4d012aedd84165bbc55c2de6d0ff21
Rahim Sonawalla <rsonawalla@gmail.com>
2013-01-20 17:17:00 -0800

    Version bump

--- soco.py
+++ soco.py
@@ -4,7 +4,7 @@
 
 # Will be parsed by setup.py to determine package metadata
 __author__ = 'Rahim Sonawalla <rsonawalla@gmail.com>'
-__version__ = '0.1'
+__version__ = '0.2'
 __website__ = 'https://github.com/rahims/SoCo'
 __license__ = 'MIT License'
 
