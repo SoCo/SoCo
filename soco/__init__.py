c2c888c34b05ef71bea8c86957690c67d51cdc07
Stefan Kögl <stefan@skoegl.net>
2013-03-16 15:40:23 +0100

    move soco.py to soco/core.py
    the __all__ in __init__.py ensures that all imports continue to work

---
+++ soco/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+""" SoCo (Sonos Controller) is a simple library to control Sonos speakers """
+
+# Will be parsed by setup.py to determine package metadata
+__author__ = 'Rahim Sonawalla <rsonawalla@gmail.com>'
+__version__ = '0.5'
+__website__ = 'https://github.com/rahims/SoCo'
+__license__ = 'MIT License'
+
+
+from core import SonosDiscovery, SoCo
+
+__all__ = ['SonosDiscovery', 'SoCo']
