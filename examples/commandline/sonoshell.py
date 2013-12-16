9b3c4bec88b8c1babefe183b12435eba7ce32cdc
Petter Aas <aaspet@gmail.com>
2013-12-16 18:31:54 +0100

    Split out some spaghetti to two functions  - adjust_volume  - get_volume_adjustment_factor

--- examples/commandline/sonoshell.py
+++ examples/commandline/sonoshell.py
@@ -5,6 +5,35 @@ import sys
 from soco import SoCo
 from soco import SonosDiscovery
 
+
+def adjust_volume(sonos, operator):
+    """ Adjust the volume up or down with a factor from 1 to 100 """
+    volume = sonos.volume()
+    factor = get_volume_adjustment_factor(operator)
+    volume = sonos.volume()
+
+    if (operator.find('+') != -1):
+        if (volume + factor) > 100:
+            factor = 1
+        print sonos.volume(volume + factor)
+        print sonos.volume()
+    elif (operator.find('-') != -1):
+        if (volume - factor) < 0:
+            factor = 1
+        print sonos.volume(volume - factor)
+        print sonos.volume()
+    else:
+        print "Valid operators for volume are + and -"
+
+
+def get_volume_adjustment_factor(operator):
+    """ get the factor to adjust the volume with """
+    factor = 1
+    if len(operator) > 1:
+        factor = int(''.join(n for n in operator if n.isdigit()))
+    return factor
+
+
 if __name__ == '__main__':
     if (len(sys.argv) > 4 or len(sys.argv) < 3):
         print "Usage: sonoshell.py [speaker's IP|all] [cmd]"
@@ -46,24 +75,9 @@ if __name__ == '__main__':
         elif (cmd == 'volume'):
             if (len(sys.argv) > 3):
                 operator = sys.argv[3].lower()
-                volume = sonos.volume()
-                factor = 1
-                if len(operator) > 1:
-                    factor = int(''.join(n for n in operator if n.isdigit()))
-                if (operator.find('+') != -1):
-                    if (volume + factor) > 100:
-                        factor = 1
-                    print sonos.volume(volume + factor)
-                    print sonos.volume()
-                elif (operator.find('-') != -1):
-                    if (volume - factor) < 0:
-                        factor = 1
-                    print sonos.volume(volume - factor)
-                    print sonos.volume()
-                else:
-                    print "Valid operators for volume are + and -"
+                adjust_volume(sonos, operator)
             else:
                 print sonos.volume()
         else:
-            print "Valid commands (with IP): info, play, pause, stop, next, previous, current, and partymode"
+            print "Valid commands (with IP): info, play, pause, stop, next, previous, current, volume and partymode"
 
