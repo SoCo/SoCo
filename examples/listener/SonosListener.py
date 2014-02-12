71b178ce2814723a036a09d67a2af70c129ee520
Stefan A Reinsberg <stefan@phas.ubc.ca>
2014-02-12 01:47:12 -0800

    conform to pylint's wishes

--- examples/listener/SonosListener.py
+++ examples/listener/SonosListener.py
@@ -5,11 +5,11 @@ import HTMLParser
 import sys
 
 def callback(data):
-    p=HTMLParser.HTMLParser()
+    p = HTMLParser.HTMLParser()
     datastr = p.unescape(data)
     root = lxml.etree.fromstring(datastr)
     r = root.xpath('//d:TransportState',
-                         namespaces={'d':"urn:schemas-upnp-org:metadata-1-0/AVT/"})
+                   namespaces={'d':"urn:schemas-upnp-org:metadata-1-0/AVT/"})
     stat = r[0].get('val')
     if stat == 'PLAYING':
         print('zone is playing')
@@ -20,15 +20,15 @@ def callback(data):
 if __name__ == '__main__':
     # find the rooms
     ips = soco.SonosDiscovery().get_speaker_ips()
-    zone_names ={}
+    zone_names = {}
     for ip in ips:
-        x=soco.SoCo(ip)
+        x = soco.SoCo(ip)
         zone_names[ip] = x.get_speaker_info()['zone_name']
 
     # identify the room to listen to
     if len(sys.argv) < 2:
         print('specify which zone to listen to')
-        for k,v in zone_names.iteritems():
+        for k, v in zone_names.iteritems():
             print(k, v)
     else:
         print(str(sys.argv))
--- soco/events.py
+++ soco/events.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
-
+# pylint: disable=W0511
+""" The events module contains the Events class for the implementation of
+the listening feature. A user-provided handler can be called upon callbacks
+from the Sonos Zones
+"""
 import socket
 import requests
 from gevent import pywsgi
 
 
 class Events(object):
+    """A class for creation and subscription to Sonos notifications
+
+    Public functions:
+    subscribe -- assign a callback function to the listener
+    start -- start the listening (i.e. send the SUBSCRIBE verb to zoneplayer)
+    stop -- shutdown the server
+    """
+
     AVTRANSPORT_ENDPOINT = 'http://{0}:1400/MediaRenderer/AVTransport/Event'
 
     def __init__(self, speaker_ip):
@@ -14,13 +26,18 @@ class Events(object):
         self.speaker_ip = speaker_ip
 
     def subscribe(self, callback):
+        """ Adds the function handle callback to the set of listeners """
         self.listeners.add(callback)
 
     def start(self, host='', port=8080):
+        """ start the server and listen to messages from zoneplayers 
+
+        TODO: check if port available and switch to other port if needed
+        """
         self.server = pywsgi.WSGIServer((host, port), self.__event_server)
         self.server.start()
 
-        ip = self.__get_local_ip()
+        ip = __get_local_ip() # pylint: disable=C0103
 
         headers = {
             'Callback': '<http://{0}:{1}>'.format(ip, port),
@@ -30,16 +47,21 @@ class Events(object):
         endpoint = self.AVTRANSPORT_ENDPOINT.format(self.speaker_ip)
 
         # `SUBSCRIBE` is a custom HTTP/1.1 verb used by Sonos devices.
+        # pylint: disable=C0103
         r = requests.request('SUBSCRIBE', endpoint, headers=headers)
 
         # Raise an exception if we get back a non-200 from the speaker.
         r.raise_for_status()
 
     def stop(self):
-        # TODO: Investigate if there is an `UNSUBSCRIBE` verb.
+        """ Stop the server listening for Zoneplayer messages
+
+        TODO: Investigate if there is an `UNSUBSCRIBE` verb.
+        """
         self.server.stop()
 
     def __event_server(self, environ, start_response):
+        """ call ALL callbacks previously hooked up through subscribe() """
         status = '405 Method Not Allowed'
 
         # `NOTIFY` is a custom HTTP/1.1 verb used by Sonos devices
@@ -66,18 +88,20 @@ class Events(object):
         start_response(status, headers)
         return [response]
 
-    def __get_local_ip(self):
-        # Not a fan of this, but there isn't a good cross-platform way of
-        # determining the local IP.
-        # From http://stackoverflow.com/a/7335145
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def __get_local_ip():
+    """ Determine local IP in a platform independent way """
+    # Not a fan of this, but there isn't a good cross-platform way of
+    # determining the local IP.
+    # From http://stackoverflow.com/a/7335145
+    # pylint: disable=C0103
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        try:
-            s.connect(('8.8.8.8', 9))
-            ip = s.getsockname()[0]
-        except socket.error:
-            raise
-        finally:
-            del s
+    try:
+        s.connect(('8.8.8.8', 9))
+        ip = s.getsockname()[0] # pylint: disable=C0103
+    except socket.error:
+        raise
+    finally:
+        del s
 
-        return ip
+    return ip
