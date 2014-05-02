da14f16375cc82600a5aee3d4c4571ecf57c70f0
Stefan Kögl <stefan@skoegl.net>
2014-05-02 19:31:25 +0200

    Improve / cleanup setup.py (fixes #117)
    Thanks to @lawrenceakka for the suggestions!

--- setup.py
+++ setup.py
@@ -3,16 +3,27 @@
 import sys
 import re
 import warnings
-try:
-    from setuptools import setup
-    has_setuptools = True
-except ImportError:
-    from distutils.core import setup
-    has_setuptools = False
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+    # Code from here: https://pytest.org/latest/goodpractises.html
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        # we don't run integration tests which need an actual Sonos device
+        self.test_args = ['-m', 'not integration']
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
 
 src = open('soco/__init__.py').read()
 metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", src))
-docstrings = re.findall('"""([^"]*)"""', src, re.MULTILINE | re.DOTALL)
+docstrings = re.findall('"""(.*?)"""', src, re.MULTILINE | re.DOTALL)
 
 NAME = 'soco'
 
@@ -22,22 +33,33 @@ PACKAGES = (
 )
 
 REQUIREMENTS = list(open('requirements.txt'))
-
-if has_setuptools:
-    OPTIONS = {
-        'install_requires': REQUIREMENTS
-    }
-else:
-    if sys.version_info < (2, 6):
-        warnings.warn('No setuptools installed. Be sure that you have '
-                      'all dependencies installed')
-    OPTIONS = {}
-
+TEST_REQUIREMENTS = list(open('requirements-dev.txt'))
 AUTHOR_EMAIL = metadata['author']
 VERSION = metadata['version']
 WEBSITE = metadata['website']
 LICENSE = metadata['license']
 DESCRIPTION = docstrings[0]
+
+CLASSIFIERS = [
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: MIT License',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python :: 2',
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.2',
+      'Programming Language :: Python :: 3.3',
+      'Programming Language :: Python :: 3.4',
+      'Programming Language :: Python :: Implementation :: PyPy',
+      'Topic :: Home Automation',
+      'Topic :: Multimedia :: Sound/Audio',
+      'Topic :: Multimedia :: Sound/Audio :: Players',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+with open('README.rst') as file:
+    LONG_DESCRIPTION = file.read()
 
 # Extract name and e-mail ("Firstname Lastname <mail@example.org>")
 AUTHOR, EMAIL = re.match(r'(.*) <(.*)>', AUTHOR_EMAIL).groups()
@@ -50,5 +72,9 @@ setup(name=NAME,
       license=LICENSE,
       url=WEBSITE,
       packages=PACKAGES,
-      **OPTIONS
+      install_requires=REQUIREMENTS,
+      tests_require=TEST_REQUIREMENTS,
+      long_description=LONG_DESCRIPTION,
+      cmdclass = {'test': PyTest},
+      classifiers=CLASSIFIERS,
 )
