6ab129ded7e20bed800b390cc2629a7c6aa77f52
robwebset <rob@deadlyduck.com>
2014-06-03 19:05:49 +0100

    Lint wanted a black line, so I've given it a blank line

--- soco/compat.py
+++ soco/compat.py
@@ -32,6 +32,7 @@ try:  # Python 2.7+
     from logging import NullHandler  # nopep8
 except ImportError:
     import logging
+
     class NullHandler(logging.Handler):
         """Create a null handler if using Python 2.6"""
         def emit(self, record):
