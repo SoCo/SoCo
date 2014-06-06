87a25f0c85f2ac579f4442bb5c7c2501c3df7f5e
Stefan Kögl <stefan@skoegl.net>
2014-06-06 18:20:51 +0200

    Add Python 2.6 to trove classifiers

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ CLASSIFIERS = [
       'License :: OSI Approved :: MIT License',
       'Operating System :: OS Independent',
       'Programming Language :: Python :: 2',
+      'Programming Language :: Python :: 2.6',
       'Programming Language :: Python :: 2.7',
       'Programming Language :: Python :: 3',
       'Programming Language :: Python :: 3.2',
