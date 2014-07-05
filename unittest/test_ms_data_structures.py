2f5d29eed58b80a67516c63dacf937f8beba20bb
Kenneth Nielsen <k.nielsen81@gmail.com>
2014-07-05 11:21:55 +0200

    And a python 2.6 fix

--- unittest/test_ms_data_structures.py
+++ unittest/test_ms_data_structures.py
@@ -182,7 +182,7 @@ MS_PLAYLIST_SEARCH_DICT = {
 class FakeMusicService(object):
     """A fake music service"""
     def __init__(self, username):
-        self.description = 'SA_RINCON5127_{}'.format(username)
+        self.description = 'SA_RINCON5127_{0}'.format(username)
         self.service_id = 20
 
     @staticmethod
