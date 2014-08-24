a1f1f041f5a9c66a56b43b41666dd1a7337f204c
Kenneth Nielsen <k.nielsen81@gmail.com>
2014-08-24 21:20:08 +0200

    Added pylint disable messages for new pylint version, possibly pylint bug

--- soco/services.py
+++ soco/services.py
@@ -196,6 +196,7 @@ class Service(object):
 
         tags = []
         for name, value in args:
+            # pylint: disable=bad-format-string
             tag = "<{name}>{value}</{name}>".format(
                 name=name, value=escape("%s" % value, {'"': "&quot;"}))
             # % converts to unicode because we are using unicode literals.
@@ -282,6 +283,7 @@ class Service(object):
         # </s:Envelope>
 
         arguments = self.wrap_arguments(args)
+        # pylint: disable=bad-format-string
         body = self.soap_body_template.format(
             arguments=arguments, action=action, service_type=self.service_type,
             version=self.version)
