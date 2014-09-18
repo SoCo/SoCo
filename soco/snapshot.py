ead115ecd46f3786fd8f52e8042e2f0c9f8148e6
robwebset <rob@deadlyduck.com>
2014-09-18 21:31:13 +0100

    Update snapshot class

--- soco/snapshot.py
+++ soco/snapshot.py
@@ -17,6 +17,12 @@ class Snapshot(object):
     Note: This does not change anything to do with the configuration
     such as which group the speaker is in, just settings that impact
     what is playing, or how it is played.
+
+    List of sources that may be playing using root of media_uri:
+    'x-rincon-queue': playing from Queue
+    'x-sonosapi-stream': playing a stream (eg radio)
+    'x-file-cifs': playing file
+    'x-rincon': slave zone (only change volume etc. rest from coordinator)
     """
 
     def __init__(self, device, snapshot_queue=False):
@@ -24,6 +30,9 @@ class Snapshot(object):
 
         :params device: Device to snapshot
         :params snapshot_queue: If the queue is to be snapshotted
+
+        :return is_coordinator (Boolean)- tells users if to play alert
+                playing an alert on a slave will un group it!
 
         Note: It is strongly advised that you do not snapshot the
         queue unless you really need to as it takes a very long
@@ -34,17 +43,27 @@ class Snapshot(object):
         self.device = device
 
         # The values that will be stored
+        # For all zones:
+        self.media_uri = None
+        self.is_coordinator = False
+        self.is_playing_queue = False
+
         self.volume = None
         self.mute = None
         self.bass = None
         self.treble = None
         self.loudness = None
+
+        # For coordinator zone playing from Queue:
         self.play_mode = None
         self.cross_fade = None
         self.playlist_position = None
         self.track_position = None
-        self.stream_uri = None
-        self.metadata = None
+
+        # For coordinator zone playing a Stream:
+        self.media_metadata = None
+
+        # For all coordinator zones
         self.transport_state = None
 
         self.queue = None
@@ -56,85 +75,141 @@ class Snapshot(object):
         """ Record and store the current state of a device
 
         """
+        # Get information about the currently playing media
+        media_info = self.device.avTransport.GetMediaInfo([('InstanceID', 0)])
+        self.media_uri = media_info['CurrentURI']
+
+        # extract source from media uri
+        if self.media_uri.split(':')[0] != 'x-rincon':
+            self.is_coordinator = True
+        if self.media_uri.split(':')[0] == 'x-rincon-queue':
+            self.is_playing_queue = True
+
         # Save the volume, mute and other sound settings
         self.volume = self.device.volume
         self.mute = self.device.mute
         self.bass = self.device.bass
         self.treble = self.device.treble
         self.loudness = self.device.loudness
-        # Save things like repeat, random, crossfade
-        self.play_mode = self.device.play_mode
-        self.cross_fade = self.device.cross_fade
 
-        # Get information about the currently playing track
-        track_info = self.device.get_current_track_info()
-        if track_info is not None:
-            self.playlist_position = track_info['playlist_position']
-            self.track_position = track_info['position']
+        # get details required for what's playing:
+        if self.is_playing_queue:
+            # playing from queue - save repeat, random, cross fade, track, etc.
+            self.play_mode = self.device.play_mode
+            self.cross_fade = self.device.cross_fade
 
-            # Check if this is a radio stream
-            if track_info['duration'] == '0:00:00':
-                self.stream_uri = track_info['uri']
-                self.metadata = track_info['metadata']
+            # Get information about the currently playing track
+            track_info = self.device.get_current_track_info()
+            if track_info is not None:
+                position = track_info['playlist_position']
+                if position != "":
+                    # save as integer
+                    self.playlist_position = int(position)
+                self.track_position = track_info['position']
+        else:
+            # playing from a stream - save media metadata
+            self.media_metadata = media_info['CurrentURIMetaData']
 
-        # Work out what the playing state is
-        transport_info = self.device.get_current_transport_info()
-        if transport_info is not None:
-            self.transport_state = transport_info['current_transport_state']
+        # Work out what the playing state is - if a coordinator
+        if self.is_coordinator:
+            transport_info = self.device.get_current_transport_info()
+            if transport_info is not None:
+                self.transport_state = transport_info[
+                    'current_transport_state']
 
         # Save of the current queue if we need to
         self._save_queue()
 
-    def restore(self):
+        # return if device is a coordinator (helps usage)
+        return self.is_coordinator
+
+    def restore(self, fade=False):
         """ Restores the state of a device that was previously saved
 
+            For coordinator devices restore everything
+            For slave devices only restore volume etc. not transport info
+            (transport info comes from the slaves coordinator).
         """
-        # Start by ensuring that the speaker is paused as we don't want
-        # things all rolling back when we are changing them, as this could
-        # include things like audio
-        transport_info = self.device.get_current_transport_info()
-        if transport_info is not None:
-            if transport_info['current_transport_state'] == 'PLAYING':
-                self.device.pause()
 
-        # Check if the queue should be restored
-        self._restore_queue()
+        if self.is_coordinator:
+            # Start by ensuring that the speaker is paused as we don't want
+            # things all rolling back when we are changing them, as this could
+            # include things like audio
+            transport_info = self.device.get_current_transport_info()
+            if transport_info is not None:
+                if transport_info['current_transport_state'] == 'PLAYING':
+                    self.device.pause()
 
-        # If a radio stream, then play that
-        if self.stream_uri is not None:
-            if self.stream_uri != "":
-                self.device.play_uri(self.stream_uri, self.metadata, False)
-        else:
-            # Not radio stream, so must be playing from playlist
-            if self.playlist_position is not None:
-                if self.playlist_position != "":
-                    position = int(self.playlist_position)
+            # Check if the queue should be restored
+            self._restore_queue()
+
+            # Reinstate what was playing
+
+            if self.is_playing_queue and self.playlist_position > 0:
+                # was playing from playlist
+                # reinstate track, position, play mode, cross fade
+                self.device.play_mode = self.play_mode
+                self.device.cross_fade = self.cross_fade
+
+                if self.playlist_position is not None:
+
                     # The position in the playlist returned by
                     # get_current_track_info starts at 1, but when
                     # playing from playlist, the index starts at 0
-                    if position > 0:
-                        position = position - 1
-                        self.device.play_from_queue(position, False)
+                    # if position > 0:
+                    self.playlist_position -= 1
+                    self.device.play_from_queue(self.playlist_position, False)
 
-            if self.track_position is not None:
-                if self.track_position != "":
-                    self.device.seek(self.track_position)
+                if self.track_position is not None:
+                    if self.track_position != "":
+                        self.device.seek(self.track_position)
+            else:
+                # was playing a stream (radio station, file, or nothing)
+                # reinstate uri and meta data
+                if self.media_uri != "":
+                    self.device.play_uri(
+                        self.media_uri, self.media_metadata, False)
 
-        # Reset all the properties that are pretty easy to do
-        self.device.volume = self.volume
+        # For all devices:
+        # Reinstate all the properties that are pretty easy to do
         self.device.mute = self.mute
         self.device.bass = self.bass
         self.device.treble = self.treble
         self.device.loudness = self.loudness
-        self.device.play_mode = self.play_mode
-        self.device.cross_fade = self.cross_fade
+
+        # Reinstate volume
+        # Can only change volume on device with fixed volume set to False
+        # otherwise get uPnP error, so check first. Before issuing a network
+        # command to check, fixed volume always has volume set to 100.
+        # So only checked fixed volume if volume is 100.
+        if self.volume == 100:
+            fixed_vol = self.device.renderingControl.GetOutputFixed(
+                [('InstanceID', 0)])['CurrentFixed']
+        else:
+            fixed_vol = False
+
+        # now set volume if not fixed
+        if not fixed_vol:
+            if fade:
+                # if fade requested in restore
+                # set volume to 0 then fade up to saved volume (non blocking)
+                self.device.volume = 0
+                self.device.renderingControl.RampToVolume(
+                    [('InstanceID', 0), ('Channel', 'Master'),
+                     ('RampType', 'SLEEP_TIMER_RAMP_TYPE'),
+                     ('DesiredVolume', self.volume),
+                     ('ResetVolumeAfter', False), ('ProgramURI', '')])
+            else:
+                # set volume
+                self.device.volume = self.volume
 
         # Now everything is set, see if we need to be playing, stopped
-        # or paused
-        if self.transport_state == 'PLAYING':
-            self.device.play()
-        elif self.transport_state == 'STOPPED':
-            self.device.stop()
+        # or paused ( only for coordinators)
+        if self.is_coordinator:
+            if self.transport_state == 'PLAYING':
+                self.device.play()
+            elif self.transport_state == 'STOPPED':
+                self.device.stop()
 
     def _save_queue(self):
         """ Saves the current state of the queue
