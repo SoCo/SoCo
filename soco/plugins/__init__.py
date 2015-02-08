2cbe3692da202b242dcb49320d44d785f06c91ba
Lawrence Akka <lakka@fake.email>
2015-02-08 21:37:05 +0000

    Whitespace and pylint fixes for new pylint

--- soco/plugins/__init__.py
+++ soco/plugins/__init__.py
@@ -17,7 +17,7 @@ class SoCoPlugin(object):
 
     def __init__(self, soco):
         cls = self.__class__.__name__
-        _LOG.info('Initializing SoCo plugin {cls}'.format(cls=cls))
+        _LOG.info('Initializing SoCo plugin %s', cls)
         self.soco = soco
 
     @property
@@ -29,7 +29,7 @@ class SoCoPlugin(object):
     def from_name(cls, fullname, soco, *args, **kwargs):
         """ Instantiate a plugin by its full name """
 
-        _LOG.info('Loading plugin {fullname}'.format(fullname=fullname))
+        _LOG.info('Loading plugin %s', fullname)
 
         parts = fullname.split('.')
         modname = '.'.join(parts[:-1])
@@ -38,6 +38,6 @@ class SoCoPlugin(object):
         mod = importlib.import_module(modname)
         cls = getattr(mod, clsname)
 
-        _LOG.info('Loaded class {cls}'.format(cls=cls))
+        _LOG.info('Loaded class %s', cls)
 
         return cls(soco, *args, **kwargs)
