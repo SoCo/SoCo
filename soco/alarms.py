67ba13f82f5a7b0ce294247cab62fde1053b4548
Lawrence Akka <lakka@fake.email>
2015-09-14 22:19:03 +0100

    Update docstrings:alarms

--- soco/alarms.py
+++ soco/alarms.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-"""Sonos Alarms."""
+"""This module contains classes relating to Sonos Alarms."""
 
 from __future__ import unicode_literals
 
@@ -17,34 +17,34 @@ TIME_FORMAT = "%H:%M:%S"
 
 
 def is_valid_recurrence(text):
-    """Check that text is a valid recurrence string.
+    """Check that `text` is a valid recurrence string.
 
-    A valid recurrence string is  'DAILY', 'ONCE', 'WEEKDAYS', 'WEEKENDS' or
-    of the form 'ON_DDDDDD' where D is a number from 0-7 representing a day
-    of the week (Sunday is 0), e.g. 'ON_034' meaning Sunday, Wednesday and
-    Thursday
+    A valid recurrence string is  ``DAILY``, ``ONCE``, ``WEEKDAYS``,
+    ``WEEKENDS`` or of the form ``ON_DDDDDD`` where ``D`` is a number from 0-7
+    representing a day of the week (Sunday is 0), e.g. ``ON_034`` meaning
+    Sunday, Wednesday and Thursday
 
-    Arg:
-        text(str): the recurrence string to check
+    Args:
+        text(str): the recurrence string to check.
 
     Returns:
-        bool: True if the recurrence string is valid, else False
+        bool: `True` if the recurrence string is valid, else `False`.
 
     Examples:
-        ::
-            >>> from soco.alarms import is_valid_recurrence
-            >>> is_valid_recurrence('WEEKENDS')
-            True
-            >>> is_valid_recurrence('')
-            False
-            >>> is_valid_recurrence('ON_132')  # Mon, Tue, Wed
-            True
-            >>> is_valid_recurrence('ON_777')  # Sat
-            True
-            >>> is_valid_recurrence('ON_3421') # Mon, Tue, Wed, Thur
-            True
-            >>> is_valid_recurrence('ON_123456789') # Too many digits
-            False
+
+        >>> from soco.alarms import is_valid_recurrence
+        >>> is_valid_recurrence('WEEKENDS')
+        True
+        >>> is_valid_recurrence('')
+        False
+        >>> is_valid_recurrence('ON_132')  # Mon, Tue, Wed
+        True
+        >>> is_valid_recurrence('ON_777')  # Sat
+        True
+        >>> is_valid_recurrence('ON_3421') # Mon, Tue, Wed, Thur
+        True
+        >>> is_valid_recurrence('ON_123456789') # Too many digits
+        False
     """
     if text in ("DAILY", "ONCE", "WEEKDAYS", "WEEKENDS"):
         return True
@@ -59,26 +59,26 @@ class Alarm(object):
     system. An alarm is not automatically saved. Call `save()` to do that.
 
     Example:
-        .. code-block::
 
-            >>> # create an alarm with default properties
-            >>> alarm = Alarm(my_device)
-            >>> print alarm.volume
-            20
-            >>> print get_alarms()
-            set([])
-            >>> # save the alarm to the Sonos system
-            >>> alarm.save()
-            >>> print get_alarms()
-            set([<Alarm id:88@15:26:15 at 0x107abb090>])
-            >>> # update the alarm
-            >>> alarm.recurrence = "ONCE"
-            >>> # Save it again for the change to take effect
-            >>> alarm.save()
-            >>> # Remove it
-            >>> alarm.remove()
-            >>> print get_alarms()
-            set([])
+        >>> device = soco.discovery.any_soco()
+        >>> # create an alarm with default properties
+        >>> alarm = Alarm(device)
+        >>> print alarm.volume
+        20
+        >>> print get_alarms()
+        set([])
+        >>> # save the alarm to the Sonos system
+        >>> alarm.save()
+        >>> print get_alarms()
+        set([<Alarm id:88@15:26:15 at 0x107abb090>])
+        >>> # update the alarm
+        >>> alarm.recurrence = "ONCE"
+        >>> # Save it again for the change to take effect
+        >>> alarm.save()
+        >>> # Remove it
+        >>> alarm.remove()
+        >>> print get_alarms()
+        set([])
     """
     # pylint: disable=too-many-instance-attributes
 
@@ -92,47 +92,54 @@ class Alarm(object):
             play_mode='NORMAL', volume=20, include_linked_zones=False):
         """
         Args:
-            zone (SoCo): The soco instance which will play the alarm.
-            start_time (datetime.time, optional): The alarm's start time.
+            zone (`SoCo`): The soco instance which will play the alarm.
+            start_time (`datetime.time`, optional): The alarm's start time.
                 Specify hours, minutes and seconds only. Defaults to the
-                current time
-            duration (datetime.time, optional): The alarm's duration. Specify
-                hours, minutes and seconds only. May be None for unlimited
-                duration. Defaults to None
-            recurrence (str, optional): A string representing how often the
-                alarm should be triggered. Can be 'DAILY', 'ONCE', 'WEEKDAYS',
-                'WEEKENDS' or of the form 'ON_DDDDDD' where D is a number from
-                0-7 representing a day of the week (Sunday is 0), e.g. 'ON_034'
-                meaning Sunday, Wednesday and Thursday. Defaults to 'DAILY'
-            enabled (bool, optional): True if alarm is enabled, False
-                otherwise. Defaults to True
-            program_uri(str, optional): The uri to play. If None, the built-in
-                Sonos chime sound will be used. Defaults to None
+                current time.
+            duration (`datetime.time`, optional): The alarm's duration. Specify
+                hours, minutes and seconds only. May be `None` for unlimited
+                duration. Defaults to `None`.
+            recurrence (str, optional): A string representing how
+                often the alarm should be triggered. Can be ``DAILY``,
+                `ONCE``, ``WEEKDAYS``, ``WEEKENDS`` or of the form
+                ``ON_DDDDDD`` where ``D`` is a number from 0-7 representing a
+                day of the week (Sunday is 0), e.g. ``ON_034`` meaning Sunday,
+                Wednesday and Thursday. Defaults to ``DAILY``.
+            enabled (bool, optional): `True` if alarm is enabled, `False`
+                otherwise. Defaults to `True`.
+            program_uri(str, optional): The uri to play. If `None`, the
+                built-in Sonos chime sound will be used. Defaults to `None`.
             program_metadata (str, optional): The metadata associated with
-                 program_uri. Defaults to ''
+                `program_uri`. Defaults to ''.
             play_mode(str, optional): The play mode for the alarm. Can be one
-                of 'NORMAL', 'SHUFFLE_NOREPEAT', 'SHUFFLE', 'REPEAT_ALL'.
-                Defaults to 'NORMAL'
-            volume (int, optional): The alarm's volume (0-100). Defaults to 20
-            include_linked_zones (bool, optional): True if the alarm should be
-                played on the other speakers in the same group, False
-                otherwise. Defaults to False
+                of ``NORMAL``, ``SHUFFLE_NOREPEAT``, ``SHUFFLE``,
+                ``REPEAT_ALL``. Defaults to ``NORMAL``.
+            volume (int, optional): The alarm's volume (0-100). Defaults to 20.
+            include_linked_zones (bool, optional): `True` if the alarm should
+                be played on the other speakers in the same group, `False`
+                otherwise. Defaults to `False`.
         """
 
         super(Alarm, self).__init__()
         self.zone = zone
         if start_time is None:
             start_time = datetime.now().time()
+        #: datetime.time: The alarm's start time.
         self.start_time = start_time
+        #: datetime.time: The alarm's duration.
         self.duration = duration
         self._recurrence = recurrence
+        #: bool: `True` if the alarm is enabled, else `False`.
         self.enabled = enabled
+        #:
         self.program_uri = program_uri
+        #: str: The uri to play.
         self.program_metadata = program_metadata
         self._play_mode = play_mode
         self._volume = volume
+        #: bool: `True` if the alarm should be played on the other speakers in
+        #  the same group, `False` otherwise.
         self.include_linked_zones = include_linked_zones
-
         self._alarm_id = None
 
     def __repr__(self):
@@ -142,16 +149,17 @@ class Alarm(object):
 
     @property
     def play_mode(self):
-        """The play mode for the alarm.
+        """
+        str: The play mode for the alarm.
 
-        Can be one of 'NORMAL', 'SHUFFLE_NOREPEAT', 'SHUFFLE',
-        'REPEAT_ALL'.
+            Can be one of ``NORMAL``, ``SHUFFLE_NOREPEAT``, ``SHUFFLE``,
+            ``REPEAT_ALL``.
         """
         return self._play_mode
 
     @play_mode.setter
     def play_mode(self, play_mode):
-        """Set the play mode."""
+        """See `playmode`."""
         play_mode = play_mode.upper()
         if play_mode not in PLAY_MODES:
             raise KeyError("'%s' is not a valid play mode" % play_mode)
@@ -159,30 +167,30 @@ class Alarm(object):
 
     @property
     def volume(self):
-        """The alarm's volume (0-100)."""
+        """int: The alarm's volume (0-100)."""
         return self._volume
 
     @volume.setter
     def volume(self, volume):
-        """Set the volume."""
+        """See `volume`."""
         # max 100
         volume = int(volume)
         self._volume = max(0, min(volume, 100))  # Coerce in range
 
     @property
     def recurrence(self):
-        """A string representing how often the alarm should be triggered.
+        """str: How often the alarm should be triggered.
 
-        Can be 'DAILY', 'ONCE', 'WEEKDAYS', 'WEEKENDS' or of the form
-        'ON_DDDDDDD' where D is a number from 0-7 representing a day of the
-        week (Sunday is 0), e.g. 'ON_034' meaning Sunday, Wednesday and
+        Can be ``DAILY``, ``ONCE``, ``WEEKDAYS``, ``WEEKENDS`` or of the form
+        ``ON_DDDDDDD`` where ``D`` is a number from 0-7 representing a day of
+        the week (Sunday is 0), e.g. ``ON_034`` meaning Sunday, Wednesday and
         Thursday.
         """
         return self._recurrence
 
     @recurrence.setter
     def recurrence(self, recurrence):
-        """Set the recurrence."""
+        """See `recurrence`."""
         if not is_valid_recurrence(recurrence):
             raise KeyError("'%s' is not a valid recurrence value" % recurrence)
 
@@ -192,8 +200,8 @@ class Alarm(object):
         """Save the alarm to the Sonos system.
 
         Raises:
-            SoCoUPnPError if the alarm cannot be created because there is
-                already an alarm for this room at the specified time
+            `SoCoUPnPException`: if the alarm cannot be created because there
+                is already an alarm for this room at the specified time.
         """
         # pylint: disable=bad-continuation
         args = [
@@ -220,11 +228,10 @@ class Alarm(object):
             self.zone.alarmClock.UpdateAlarm(args)
 
     def remove(self):
-        """Removes the alarm.
+        """Remove the alarm from the Sonos system.
 
-        Removes the alarm from the Sonos system. There is no need to
-        call `save`. The Python instance is not deleted, and can be
-        saved back to Sonos again if desired.
+        There is no need to call `save`. The Python instance is not deleted,
+        and can be saved back to Sonos again if desired.
         """
         self.zone.alarmClock.DestroyAlarm([
             ('ID', self._alarm_id)
@@ -241,14 +248,14 @@ def get_alarms(soco=None):
     """Get a set of all alarms known to the Sonos system.
 
     Args:
-        soco (SoCo, optional): a SoCo instance to query. If None, a random
-        instance is used. Defaults to None
+        soco (`SoCo`, optional): a SoCo instance to query. If None, a random
+            instance is used. Defaults to `None`.
 
     Returns:
-        set: A set of Alarm instances
+        set: A set of `Alarm` instances
 
     Note:
-        Any existing Alarm instance will have its attributes updated to those
+        Any existing `Alarm` instance will have its attributes updated to those
         currently stored on the Sonos system.
     """
     # Get a soco instance to query. It doesn't matter which.
