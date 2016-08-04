6bb475a79f82cc83e8564d9639f468b9d0dda2ed
Kenneth Nielsen <k.nielsen81@gmail.com>
2016-08-04 20:48:26 +0200

    Update flake8 line disables after they decided on noqa instead of nopep8

--- soco/compat.py
+++ soco/compat.py
@@ -9,35 +9,35 @@ It is used internally by SoCo to ensure compatibility with Python 2."""
 from __future__ import unicode_literals
 
 try:  # python 3
-    from http.server import SimpleHTTPRequestHandler  # nopep8
-    from urllib.request import urlopen  # nopep8
-    from urllib.error import URLError  # nopep8
-    from urllib.parse import quote_plus  # nopep8
-    import socketserver  # nopep8
-    from queue import Queue  # nopep8
-    StringType = bytes  # nopep8
-    UnicodeType = str  # nopep8
-    from urllib.parse import quote as quote_url  # nopep8
-    from urllib.parse import urlparse, parse_qs  # nopep8
+    from http.server import SimpleHTTPRequestHandler  # noqa
+    from urllib.request import urlopen  # noqa
+    from urllib.error import URLError  # noqa
+    from urllib.parse import quote_plus  # noqa
+    import socketserver  # noqa
+    from queue import Queue  # noqa
+    StringType = bytes  # noqa
+    UnicodeType = str  # noqa
+    from urllib.parse import quote as quote_url  # noqa
+    from urllib.parse import urlparse, parse_qs  # noqa
 
 except ImportError:  # python 2.7
-    from SimpleHTTPServer import SimpleHTTPRequestHandler  # nopep8
-    from urllib2 import urlopen, URLError  # nopep8
-    from urllib import quote_plus  # nopep8
-    import SocketServer as socketserver  # nopep8
-    from Queue import Queue  # nopep8
-    from types import StringType, UnicodeType  # nopep8
-    from urllib import quote as quote_url   # nopep8
-    from urlparse import urlparse, parse_qs  # nopep8
+    from SimpleHTTPServer import SimpleHTTPRequestHandler  # noqa
+    from urllib2 import urlopen, URLError  # noqa
+    from urllib import quote_plus  # noqa
+    import SocketServer as socketserver  # noqa
+    from Queue import Queue  # noqa
+    from types import StringType, UnicodeType  # noqa
+    from urllib import quote as quote_url   # noqa
+    from urlparse import urlparse, parse_qs  # noqa
 
 try:  # python 2.7 - this has to be done the other way round
-    from cPickle import dumps  # nopep8
+    from cPickle import dumps  # noqa
 except ImportError:  # python 3
-    from pickle import dumps  # nopep8
+    from pickle import dumps  # noqa
 
 # Support Python 2.6
 try:  # Python 2.7+
-    from logging import NullHandler  # nopep8
+    from logging import NullHandler  # noqa
 except ImportError:
     import logging
 
