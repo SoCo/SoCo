ee4b20eec6c48bf0de46401d925bc6e5df10a67c
Lewis Juggins <ldjuggins@gmail.com>
2017-01-02 12:04:51 +0000

    Fix version

--- soco/__init__.py
+++ soco/__init__.py
@@ -17,7 +17,7 @@ from .exceptions import SoCoException, UnknownSoCoException
 
 # Will be parsed by setup.py to determine package metadata
 __author__ = 'The SoCo-Team <python-soco@googlegroups.com>'
-__version__ = '0.12.0'
+__version__ = '0.12'
 __website__ = 'https://github.com/SoCo/SoCo'
 __license__ = 'MIT License'
 
