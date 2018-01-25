940d2f97599bc9a3979e843c5bf3f7c7e2f1c8ef
Kenneth Nielsen <k.nielsen81@gmail.com>
2018-01-25 13:24:32 +0100

    Revised play_local_files examples with command line arguments and zone checks

--- examples/play_local_files/play_local_files.py
+++ examples/play_local_files/play_local_files.py
@@ -5,8 +5,14 @@ To use the script:
  * Make sure soco is installed
  * Drop this script into a folder that, besides python files, contains
 nothing but music files
- * Adjust the settings on the first three lines of the main function
- * Run the script
+ * Choose which player to use and run the script at the command line as such:
+
+play_local_files.py "Living Room"
+
+NOTE: The script has been changed from the earlier version, where the
+settings were written directly into the file. They now have to be
+given at the command line instead. But, it should only be necessary to
+supply the zone name. The local machine IP should be autodetected.
 
 """
 
@@ -14,7 +20,9 @@ nothing but music files
 from __future__ import print_function, unicode_literals
 
 import os
+import sys
 import time
+import socket
 from threading import Thread
 from random import choice
 try:
@@ -30,7 +38,7 @@ except ImportError:
     from SocketServer import TCPServer
     print('Running as python 2')
 
-import soco
+from soco.discovery import by_name, discover
 
 
 class HttpServer(Thread):
@@ -74,21 +82,66 @@ def add_random_file_from_present_folder(machine_ip, port, zone_name):
     print('\nPlaying random file:', random_file)
     netpath = 'http://{}:{}/{}'.format(machine_ip, port, random_file)
 
-    for zone in soco.discover():
-        if zone.player_name == zone_name:
-            break
+    # Get the zone
+    zone = by_name(zone_name)
 
-    number_in_queue = zone.add_uri_to_queue(netpath)    
-    zone.play_from_queue(number_in_queue)
+    # Check is a zone by the given name was found
+    if zone is None:
+        zone_names = [zone_.player_name for zone_ in discover()]
+        print("No Sonos player names '{}'. Player names are {}"\
+              .format(zone_name, zone_names))
+        sys.exit(1)
+
+    # Check whether the zone is a coordinator (stand alone zone or
+    # master of a group)
+    if not zone.is_coordinator:
+        print("The zone '{}' is not a group master, and therefore cannot "
+              "play music. Please use '{}' in stead"\
+              .format(zone_name, zone.group.coordinator.player_name))
+        sys.exit(2)
+
+    number_in_queue = zone.add_uri_to_queue(netpath)
+    # play_from_queue indexes are 0-based
+    zone.play_from_queue(number_in_queue - 1)
+
+
+def detect_ip_address():
+    """Return the local ip-address"""
+    # Rather hackish way to get the local ip-address, recipy from
+    # https://stackoverflow.com/a/166589
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip_address = s.getsockname()[0]
+    s.close()
+    return ip_address
+
+
+def parse_args():
+    """Parse the command line arguments"""
+    import argparse
+    description = 'Play local files with Sonos by running a local web server'
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument('zone', help='The name of the zone to play from')
+    parser.add_argument('--port', default=8000,
+                        help='The local machine port to run the webser on')
+    parser.add_argument('--ip', default=detect_ip_address(),
+                        help='The local IP address of this machine. By '
+                        'default it will attempt to autodetect it.')
+
+    return parser.parse_args()
 
 
 def main():
     # Settings
-    machine_ip = '192.168.0.25'
-    port = 8000
-    zone_name = 'Stue'  # Danish for living room
+    args = parse_args()
+    print(" Will use the following settings:\n"
+          " Zone: {args.zone}\n"
+          " IP of this machine: {args.ip}\n"
+          " Use port: {args.port}".format(args=args))
+
     # Setup and start the http server
-    server = HttpServer(port)
+    server = HttpServer(args.port)
     server.start()
 
     # When the http server is setup you can really add your files in
@@ -96,7 +149,7 @@ def main():
     # add_random_file_from_present_folder is just an example, but it may be
     # helpful in figuring out how to format the urls
     try:
-        add_random_file_from_present_folder(machine_ip, port, zone_name)
+        add_random_file_from_present_folder(args.ip, args.port, args.zone)
         # Remember the http server runs in its own daemonized thread, so it is
         # necessary to keep the main thread alive. So sleep for 3 years.
         time.sleep(10**8)
