f532cadb41179d3e030a6fc21bc32f7773070169
Eli Schleifer <1265982+EliSchleifer@users.noreply.github.com>
2019-04-24 14:29:53 -0700

    Fix for Issue 660 - Properly parse discover
    Properly parse discover return messages. Addresses 
    https://github.com/SoCo/SoCo/issues/660

--- soco/core.py
+++ soco/core.py
@@ -949,7 +949,7 @@ class SoCo(_SocoSingletonBase):
         self._all_zones.clear()
         self._visible_zones.clear()
         # Loop over each ZoneGroup Element
-        for group_element in tree.findall('ZoneGroup'):
+        for group_element in tree.find('ZoneGroups').findall('ZoneGroup'):
             coordinator_uid = group_element.attrib['Coordinator']
             group_uid = group_element.attrib['ID']
             group_coordinator = None
