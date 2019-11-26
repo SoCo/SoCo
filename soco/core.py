ac51352d70de5f67ea14c8ed4a0c347cbb57b45b
pwt <1089749+pwt@users.noreply.github.com>
2019-11-26 12:34:20 +0000

    Fix docstring error.

--- soco/core.py
+++ soco/core.py
@@ -1122,7 +1122,7 @@ class SoCo(_SocoSingletonBase):
         volume to undershoot the minimum value of 0, the volume will be set
         to 0.
 
-        This method is an alternative to using increment and decrement
+        This method is an alternative to using addition and subtraction
         assignment operators (+=, -=) on the `group_volume` property of a
         `SoCo` instance. These operators perform the same function as
         `set_relative_group_volume()` but require two network calls per
