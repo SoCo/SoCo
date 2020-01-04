0bf37bc3f5c1cc76c0376acf0e559ce11a949331
Kenneth Nielsen <k.nielsen81@gmail.com>
2020-01-04 21:02:18 +0100

    Lint fixes for soco

--- soco/music_services/data_structures.py
+++ soco/music_services/data_structures.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Disable while we have Python 2.x compatability
-# pylint: disable=useless-object-inheritance
+# pylint: disable=useless-object-inheritance, too-many-arguments
 
 """Data structures for music service items
 
