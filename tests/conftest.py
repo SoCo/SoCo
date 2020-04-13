d0adeba4fe69fc4bded2801b75a0deefc8a67b84
Kenneth Nielsen <k.nielsen81@gmail.com>
2020-04-13 14:38:38 +0200

    Add integration tests for data_structures_entry
    And add a data loader to the test utils, that we can hopefully use to move test data out of python code for many other tests as well

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,14 @@
 Add the --ip command line option, and skip all tests marked the with
 'integration' marker unless the option is included
 """
+from os import path
+import json
+import codecs
+
+
 import pytest
+
+THISDIR = path.dirname(path.abspath(__file__))
 
 
 def pytest_addoption(parser):
@@ -53,3 +60,32 @@ class Helpers(object):
 @pytest.fixture
 def helpers():
     return Helpers
+
+
+class DataLoader:
+    """A class that loads test data"""
+
+    def __init__(self, data_sub_dir):
+        self.data_dir = path.join(THISDIR, "data", data_sub_dir)
+
+    def load_xml(self, filename):
+        """Return XML string loaded from filename under ``self.data_sub_dir``"""
+        xml_string = ""
+        with codecs.open(path.join(self.data_dir, filename), encoding="utf-8") as file_:
+            for line in file_:
+                # Allow for indenting the XML source
+                xml_string += line.lstrip(" ")
+        return xml_string
+
+    def load_json(self, filename):
+        """Return parsed data from json file"""
+        with open(path.join(self.data_dir, filename)) as file_:
+            data = json.load(file_)
+        return data
+
+    def load_xml_and_json(self, filename_base):
+        """Return XML string and parsed data from ``filename_base``.xml and .json"""
+        return (
+            self.load_xml(filename_base + ".xml"),
+            self.load_json(filename_base + ".json"),
+        )
