1b40caa680a7a5558e6c0677ff94c0bea3881230
jjlawren <jjlawren@users.noreply.github.com>
2022-06-12 11:46:07 -0500

    Add Ray to known soundbars

--- soco/core.py
+++ soco/core.py
@@ -3077,7 +3077,15 @@ SOURCES = {
 }
 
 # Soundbar product names
-SOUNDBARS = ("playbase", "playbar", "beam", "sonos amp", "arc", "arc sl")
+SOUNDBARS = (
+    "arc",
+    "arc sl",
+    "beam",
+    "playbase",
+    "playbar",
+    "ray",
+    "sonos amp",
+)
 
 if config.SOCO_CLASS is None:
     config.SOCO_CLASS = SoCo
