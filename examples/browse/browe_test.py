eb129902ea7f26ed3d35c55a60c5d4117e131773
PeteRager <76050312+PeteRager@users.noreply.github.com>
2025-02-18 10:32:21 -0500

    set user agent for content directory

--- examples/browse/browe_test.py
+++ examples/browse/browe_test.py
@@ -1,15 +1,9 @@
 from soco import SoCo
-my_zone = SoCo('192.168.50.180')
+my_zone = SoCo('192.168.1.180')
 
 print("Player Name" + my_zone.player_name)
 
 print(my_zone.contentDirectory.additional_headers)
-
-my_zone.contentDirectory.additional_headers = {
-    "X-Sonos-Api-Key" : "api_key",
-    "X-Sonos-Corr-Id" : "corr_id",
-    "X-SONOS-TARGET-UDN" : "udn"
-}
 
 r = my_zone.contentDirectory.Browse([('ObjectID', 'FV:2'),
                                     ('BrowseFlag', 'BrowseDirectChildren'),
