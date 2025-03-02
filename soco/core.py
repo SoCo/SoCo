7e50781033f1371e945086c57192d64673898237
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-03-02 22:09:08 +1030

    Update core.py

--- soco/core.py
+++ soco/core.py
@@ -326,12 +326,7 @@ class SoCo(_SocoSingletonBase):
         # Note: Creation of a SoCo instance should be as cheap and quick as
         # possible. Do not make any network calls here
         super().__init__()
-        # Check if ip_address is a valid IPv4 representation.
-        # Sonos does not (yet) support IPv6
-        try:
-            socket.inet_aton(ip_address)
-        except OSError as error:
-            raise ValueError("Not a valid IP address string") from error
+
         #: The speaker's ip address
         self.ip_address = ip_address
         self.speaker_info = {}  # Stores information about the current speaker
