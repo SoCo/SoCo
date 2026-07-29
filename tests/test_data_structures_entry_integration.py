5429d7127046fa1105d173ed47bf03f8a1b2cfff
PeteRager <pete.sage@icloud.com>
2026-07-29 07:57:30 -0400

    fix format error

--- tests/test_data_structures_entry_integration.py
+++ tests/test_data_structures_entry_integration.py
@@ -131,7 +131,7 @@ def test_vendor_extended_didl_class(class_name, base_class, didl_xml_string, dat
 
 def test_from_didl_string_missing_upnp_class_raises():
     """Test that from_didl_string raises DIDLMetadataError when a DIDL item
-    has no upnp:class element. """
+    has no upnp:class element."""
     from soco.exceptions import DIDLMetadataError
 
     didl_missing_class = (
